f032de25a95d4ceec6047f182c3bbae3d2bb6e65
Alex Lowe <loweab@gmail.com>
2016-06-19 11:43:31 -0400

    ideas page, updated css

--- src/main/java/com/alexlowe/courses/Main.java
+++ src/main/java/com/alexlowe/courses/Main.java
@@ -1,5 +1,6 @@
 package com.alexlowe.courses;
 
+import com.alexlowe.courses.model.CourseIdea;
 import com.alexlowe.courses.model.CourseIdeaDAO;
 import com.alexlowe.courses.model.SimpleCourseIdeaDAO;
 import spark.ModelAndView;
@@ -20,6 +21,7 @@ public class Main {
         staticFileLocation("/public");
         CourseIdeaDAO dao = new SimpleCourseIdeaDAO(); /*interface and implementation*/
         //this implementation is just for prototyping and will not survive server restart, for persistence, need a db
+
         get("/", (req, res) -> {
             Map<String,String> model = new HashMap<>();
             model.put("username", req.cookie("username"));
@@ -34,5 +36,19 @@ public class Main {
             return new ModelAndView(model, "sign-in.hbs");
         },  new HandlebarsTemplateEngine());
 
+        get("/ideas", (req, res) -> { /* ** */
+            Map<String,Object> model = new HashMap<>();
+            model.put("ideas", dao.findAll()); /*passing in list of CourseIdeas, will use in ideas.hbs*/
+            return new ModelAndView(model, "ideas.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/ideas", (req, res) -> {
+            String title = req.queryParams("title");
+            //TODO:username is tied to cookie implementation, want to change that;
+            CourseIdea courseIdea = new CourseIdea(title, req.queryParams("username"));
+            dao.add(courseIdea);
+            res.redirect("ideas"); /*refresh page*/
+            return null;
+        },  new HandlebarsTemplateEngine());
     }
 }
