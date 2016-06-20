0593c97733161221d0c3b24af5fe23f529d6f378
Alex Lowe <loweab@gmail.com>
2016-06-20 00:24:17 -0400

    flash-messages

--- src/main/java/com/alexlowe/courses/Main.java
+++ src/main/java/com/alexlowe/courses/Main.java
@@ -2,10 +2,13 @@ package com.alexlowe.courses;
 
 import com.alexlowe.courses.model.CourseIdea;
 import com.alexlowe.courses.model.CourseIdeaDAO;
+import com.alexlowe.courses.model.NotFoundException;
 import com.alexlowe.courses.model.SimpleCourseIdeaDAO;
 import spark.ModelAndView;
+import spark.Request;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
+import java.nio.channels.NotYetBoundException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,6 +18,9 @@ import static spark.Spark.*;
  * Created by Keyes on 6/18/2016.
  */
 public class Main {
+
+    private static final String FLASH_MESSAGE_KEY = "flash_message";
+
     public static void main(String[] args) {
         staticFileLocation("/public");
         CourseIdeaDAO dao = new SimpleCourseIdeaDAO(); /*interface and implementation*/
@@ -27,8 +33,8 @@ public class Main {
         });
 
         before("/ideas", (req, res) -> {
-            //TODO: send msg about redirect
             if(req.attribute("username") == null){
+                setFlashMessage(req, "Whoops, please sign in first.");
                 res.redirect("/");
                 halt();
             }
@@ -36,6 +42,7 @@ public class Main {
         get("/", (req, res) -> {
             Map<String,String> model = new HashMap<>();
             model.put("username", req.attribute("username"));
+            model.put("flashMessage", captureFlashMessage(req));
             return new ModelAndView(model, "index.hbs");
         },  new HandlebarsTemplateEngine());
 
@@ -50,6 +57,7 @@ public class Main {
         get("/ideas", (req, res) -> { /* ** */
             Map<String,Object> model = new HashMap<>();
             model.put("ideas", dao.findAll()); /*passing in list of CourseIdeas, will use in ideas.hbs*/
+            model.put("flashMessage", captureFlashMessage(req));
             return new ModelAndView(model, "ideas.hbs");
         }, new HandlebarsTemplateEngine());
 
@@ -69,9 +77,50 @@ public class Main {
 
         post("/ideas/:slug/vote", (req, res) -> {
             CourseIdea idea = dao.findBySlug(req.params("slug")); //this pulls whats in :slug
-            idea.addVoter(req.attribute("username"));
+            boolean voteAdded = idea.addVoter(req.attribute("username"));
+            if(voteAdded){
+                setFlashMessage(req, "Thanks for your vote!");
+            }else{
+                setFlashMessage(req, "You already voted.");
+            }
             res.redirect("/ideas");
             return null;
         });
+
+        exception(NotFoundException.class, (exc, req, res) -> {
+            res.status(404);
+            HandlebarsTemplateEngine engine = new HandlebarsTemplateEngine();
+            String html = engine.render(new ModelAndView(null, "not-found.hbs"));
+            res.body(html);
+        });
+    }
+
+    private static void setFlashMessage(Request req, String msg) {
+        req.session().attribute(FLASH_MESSAGE_KEY, msg);
+    }
+
+    //make sure we don't create a session if we're not going to use it
+    private static String getFlashMessage(Request req) {
+        if (req.session(false) == null){ //no flash message to get
+            return null;
+        }
+
+        //if the session already exists, we want to make sure our key exists
+        if(!req.session().attributes().contains(FLASH_MESSAGE_KEY)){
+            return null;
+        }
+
+        //if it does exist, we can pop it out
+        return (String) req.session().attribute(FLASH_MESSAGE_KEY);
+    }
+
+    private static String captureFlashMessage(Request req) {
+        String message = getFlashMessage(req);
+
+        //see if f-m exists, and remove it if it does
+        if(message != null){
+            req.session().removeAttribute(FLASH_MESSAGE_KEY);
+        }
+        return message;
     }
 }
